63b626d42bc1f6e04a5ab42d48c21b07c9b50f8b
d-herbert <deidre.m.herbert@gmail.com>
2022-03-20 21:55:03 -0700

    inital setup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 // global variables
 let empForm
+let output
+let returnVals
 
 // helper functions
-const $ = (id) => {
-    document.getElementById(id)
-}
+// const $ = (id) => {
+//     document.getElementById(id)
+// }
 
 // DOM elements
-empForm = $('empForm')
+let submitForm = document.getElementById('submitForm')
+let digit8 = document.getElementById('digit8')
+let username = document.getElementById('username')
+let digit4 = document.getElementById('digit4')
+let emailID = document.getElementById('emailID')
+let deptSelection = document.getElementById('deptSelection')
+
+// function
+function outputResults(digit8, username, digit4, emailID, deptSelection) {
+    returnVals = `ID: ${digit8} \nName: ${username} \nExtension: ${digit4} \nEmail: ${emailID} \nDepartment: ${deptSelection}`
+    
+    return returnVals
+}
+
+submitForm.addEventListener('click', (e) => {
+    digit8 = parseInt(digit8.value)
+    username = String(username.value)
+    digit4 = parseInt(digit4.value)
+    emailID = String(emailID.value)
+    deptSelection = String(deptSelection.value)
+
+    // console.log(returnVals(digit8, username))
+    console.log(outputResults(digit8, username, digit4, emailID, deptSelection))
+    e.preventDefault()
+})
+
